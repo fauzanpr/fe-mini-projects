13ed296b8e3deccc639a20c63988ed70527d698f
fauzanpr <fauzanpr.06@gmail.com>
2023-05-20 00:56:29 +0700

    [FEAT](react-time-count) add stop feature

--- react-time-count/src/components/Counter.js
+++ react-time-count/src/components/Counter.js
@@ -21,18 +21,27 @@ const Counter = () => {
   const btnStartHandler = () => {
     setStart(true);
     document.getElementById('btn-start').classList.add('btn--inactive');
+    document.getElementById('btn-stop').classList.remove('btn--inactive');
+    document.getElementById('btn-stop').classList.add('btn--stop');
+  };
+
+  const btnStopHandler = () => {
+    setStart(false);
+    document.getElementById('btn-start').classList.remove('btn--inactive');
+    document.getElementById('btn-stop').classList.remove('btn--stop');
+    document.getElementById('btn-stop').classList.add('btn--inactive');
   };
 
   return (
     <div className="container">
       <div className="set-long-container">
         <input type="number" placeholder="How Long?" />
-        <button className="btn" onClick={ btnStartHandler } id="btn-start" >Start</button>
+        <button className="btn" onClick={ btnStartHandler } id="btn-start">Start</button>
       </div>
       <div className="count-container">
         <h1 className="count">{count}</h1>
       </div>
-      <button className="btn btn--stop">Stop</button>
+      <button className="btn btn--inactive" id="btn-stop" onClick={ btnStopHandler }>Stop</button>
     </div>
   );
 };
