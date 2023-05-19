0ae57035f2669bc141ce401411ffc455b41ef3a9
fauzanpr <fauzanpr.06@gmail.com>
2023-05-20 00:39:40 +0700

    [FEAT](react-time-count) create count

--- react-time-count/src/components/Counter.js
+++ react-time-count/src/components/Counter.js
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
+
 const Counter = () => {
+  const [start, setStart] = useState(false);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+
+    return () => {
+        clearInterval(interval);
+    }
+  }, [count]);
+
   return (
     <div className="container">
       <div className="set-long-container">
         <input type="number" placeholder="How Long?" />
-        <button className="btn">Start Count</button>
+        <button className="btn">Start</button>
       </div>
       <div className="count-container">
-        <h1 className="count">1</h1>
+        <h1 className="count">{count}</h1>
       </div>
       <button className="btn btn--stop">Stop</button>
     </div>
