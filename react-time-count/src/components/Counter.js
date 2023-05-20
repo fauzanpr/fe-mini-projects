aaa3088d1e46939b00722074c02b9e0419823852
fauzanpr <fauzanpr.06@gmail.com>
2023-05-20 10:05:34 +0700

    [FIX](react-time-count) fix max count

--- react-time-count/src/components/Counter.js
+++ react-time-count/src/components/Counter.js
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 const Counter = () => {
   const [start, setStart] = useState(false);
   const [count, setCount] = useState(0);
-
-  const inactive = false;
+  const [setedMaxCount, setSetedMaxCount] = useState(false);
+  const [maxCount, setMaxCount] = useState(0);
 
   useEffect(() => {
-    if (start) {
+    if (start && count < Number(setedMaxCount)) {
       const interval = setInterval(() => {
+        // console.log(count);
+        // console.log();
         setCount(count + 1);
       }, 1000);
 
@@ -16,32 +18,49 @@ const Counter = () => {
         clearInterval(interval);
       };
     }
-  }, [count, start]);
+  }, [count, start, setedMaxCount]);
 
   const btnStartHandler = () => {
     setStart(true);
-    document.getElementById('btn-start').classList.add('btn--inactive');
-    document.getElementById('btn-stop').classList.remove('btn--inactive');
-    document.getElementById('btn-stop').classList.add('btn--stop');
+    document.getElementById("btn-start").classList.add("btn--inactive");
+    document.getElementById("btn-stop").classList.remove("btn--inactive");
+    document.getElementById("btn-stop").classList.add("btn--stop");
   };
 
   const btnStopHandler = () => {
     setStart(false);
-    document.getElementById('btn-start').classList.remove('btn--inactive');
-    document.getElementById('btn-stop').classList.remove('btn--stop');
-    document.getElementById('btn-stop').classList.add('btn--inactive');
+    document.getElementById("btn-start").classList.remove("btn--inactive");
+    document.getElementById("btn-stop").classList.remove("btn--stop");
+    document.getElementById("btn-stop").classList.add("btn--inactive");
   };
 
   return (
     <div className="container">
       <div className="set-long-container">
-        <input type="number" placeholder="How Long?" />
-        <button className="btn" onClick={ btnStartHandler } id="btn-start">Start</button>
+        <input
+          type="number"
+          placeholder="How Long?"
+          value={maxCount}
+          onChange={(e) => {
+            setMaxCount(e.target.value);
+            console.log(maxCount);
+          }}
+        />
+        <button className="btn btn--set" onClick={ () => setSetedMaxCount(maxCount) }>Set</button>
       </div>
+      <button className="btn" onClick={btnStartHandler} id="btn-start">
+        Start
+      </button>
       <div className="count-container">
         <h1 className="count">{count}</h1>
       </div>
-      <button className="btn btn--inactive" id="btn-stop" onClick={ btnStopHandler }>Stop</button>
+      <button
+        className="btn btn--inactive"
+        id="btn-stop"
+        onClick={btnStopHandler}
+      >
+        Stop
+      </button>
     </div>
   );
 };
